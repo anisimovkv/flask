1e610c256c5c16f9db0918256ecfb28334f20aa9
konstantin <konstantin.anisimov@eosda.com>
2020-12-28 16:49:58 +0200

    added:  - blueprint application post  - blueprint template post_index.html

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 
 from config import Configuration
+from posts.blueprint import posts
 
 app: Flask = Flask(__name__)
 app.config.from_object(Configuration)
+app.register_blueprint(posts, url_prefix='/blog')
